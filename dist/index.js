fa6067f288187eb9ecfc51600a91bfe30c028ef3
marco <toyko2001@gmail.com>
2024-04-18 14:29:28 +0630

    fix(scroll): fix window scroll position

--- dist/index.js
+++ dist/index.js
@@ -4,105 +4,84 @@ import { DrawingItemTool } from "./tools/items.js";
 import { SizingTool } from "./tools/sizing.js";
 import { createElement } from "./utils.js";
 class Paint {
-  constructor(tools, backgroundOverlay) {
-    this.backgroundOverlay = backgroundOverlay;
-    this.tools = tools;
-    this.tools.canvas.props.addEventListener(
-      "mousedown",
-      this.handleMouseDown.bind(this),
-    );
-    this.tools.canvas.props.addEventListener(
-      "mousemove",
-      this.handleMouseMove.bind(this),
-    );
-    this.tools.canvas.props.addEventListener(
-      "mouseup",
-      this.handleMouseUp.bind(this),
-    );
-    this.tools.canvas.props.addEventListener(
-      "mouseout",
-      this.handleMouseOut.bind(this),
-    );
-    setTimeout(() => this.resize(), 100);
-    window.addEventListener("resize", this.resize.bind(this));
-  }
-  resize() {
-    const overlayRect = this.backgroundOverlay.getBoundingClientRect();
-    this.tools.canvas.resize({
-      width: .750,
-      height: .70,
-      top: .235,
-      left: .016,
-      overlayRect,
-    });
-    this.tools.colors.resize({
-      width: .17,
-      height: .38,
-      top: .06,
-      left: .8,
-      overlayRect: overlayRect,
-    });
-    this.tools.items.resize({
-      width: .17,
-      height: .29,
-      top: .49,
-      left: .8,
-      overlayRect: overlayRect,
-    });
-    this.tools.sizing.resize({
-      width: .17,
-      height: .06,
-      top: .88,
-      left: .8,
-      overlayRect: overlayRect,
-    });
-  }
-  static new(tools, backgroundOverlay) {
-    return new Paint(tools, backgroundOverlay);
-  }
-  handleMouseDown(evt) {
-    this.tools.canvas.handleMouseDown(evt);
-  }
-  handleMouseMove(evt) {
-    if (this.tools.canvas.ctx) {
-      this.tools.canvas.addCursor(this.tools.items.cursorUrl);
-      this.tools.canvas.ctx.strokeStyle = this.tools.colors.currentColor;
-      switch (this.tools.items.currentItem) {
-        case "eraser": {
-          this.tools.canvas.mode = "eraser";
-          break;
-        }
-        case "insert": {
-          this.tools.canvas.mode = "insert";
-          break;
-        }
-        default: {
-          this.tools.canvas.mode = "draw";
-          break;
-        }
-      }
-      if (
-        this.tools.items.currentItem !== "eraser"
-        && this.tools.items.currentItem !== "insert"
-      ) {
-        this.tools.canvas.ctx.lineCap = this.tools.items.currentItem;
-      }
-      this.tools.canvas.ctx.lineWidth = this.tools.sizing.currentSize;
+    constructor(tools, backgroundOverlay) {
+        this.backgroundOverlay = backgroundOverlay;
+        this.tools = tools;
+        this.tools.canvas.props.addEventListener("mousedown", this.handleMouseDown.bind(this));
+        this.tools.canvas.props.addEventListener("mousemove", this.handleMouseMove.bind(this));
+        this.tools.canvas.props.addEventListener("mouseup", this.handleMouseUp.bind(this));
+        this.tools.canvas.props.addEventListener("mouseout", this.handleMouseOut.bind(this));
+        setTimeout(() => this.resize(), 100);
+        window.addEventListener("resize", this.resize.bind(this));
     }
-    this.tools.canvas.handleMouseMove(evt);
-  }
-  handleMouseUp(evt) {
-    this.tools.canvas.handleMouseUp(evt);
-  }
-  handleMouseOut(evt) {
-    this.tools.canvas.handleMouseOut(evt);
-  }
+    resize() {
+        const overlayRect = this.backgroundOverlay.getBoundingClientRect();
+        this.tools.canvas.resize({
+            width: .750,
+            height: .70,
+            top: .235,
+            left: .016,
+            overlayRect,
+        });
+        this.tools.colors.resize({
+            width: .17,
+            height: .38,
+            top: .06,
+            left: .8,
+            overlayRect: overlayRect,
+        });
+        this.tools.items.resize({
+            width: .17,
+            height: .29,
+            top: .49,
+            left: .8,
+            overlayRect: overlayRect,
+        });
+        this.tools.sizing.resize({
+            width: .17,
+            height: .06,
+            top: .88,
+            left: .8,
+            overlayRect: overlayRect,
+        });
+    }
+    static new(tools, backgroundOverlay) {
+        return new Paint(tools, backgroundOverlay);
+    }
+    handleMouseDown(evt) {
+        this.tools.canvas.handleMouseDown(evt);
+    }
+    handleMouseMove(evt) {
+        if (this.tools.canvas.ctx) {
+            this.tools.canvas.addCursor(this.tools.items.cursorUrl);
+            this.tools.canvas.ctx.strokeStyle = this.tools.colors.currentColor;
+            switch (this.tools.items.currentItem) {
+                case "eraser": {
+                    this.tools.canvas.mode = "eraser";
+                    break;
+                }
+                case "insert": {
+                    this.tools.canvas.mode = "insert";
+                    break;
+                }
+                default: {
+                    this.tools.canvas.mode = "draw";
+                    break;
+                }
+            }
+            if (this.tools.items.currentItem !== "eraser"
+                && this.tools.items.currentItem !== "insert") {
+                this.tools.canvas.ctx.lineCap = this.tools.items.currentItem;
+            }
+            this.tools.canvas.ctx.lineWidth = this.tools.sizing.currentSize;
+        }
+        this.tools.canvas.handleMouseMove(evt);
+    }
+    handleMouseUp(evt) {
+        this.tools.canvas.handleMouseUp(evt);
+    }
+    handleMouseOut(evt) {
+        this.tools.canvas.handleMouseOut(evt);
+    }
 }
-export {
-  ColorTool,
-  createElement,
-  DrawableCanvas,
-  DrawingItemTool,
-  Paint,
-  SizingTool,
-};
+export { ColorTool, createElement, DrawableCanvas, DrawingItemTool, Paint, SizingTool, };
--- dist/tools/items.js
+++ dist/tools/items.js
@@ -2,67 +2,56 @@ import { AppError, ErrorKind } from "../error.js";
 import { createElement } from "../utils.js";
 import { Tool } from "./index.js";
 export class DrawingItemTool extends Tool {
-  constructor(props) {
-    super("DrawingItemTool");
-    this.props = props;
-    this.currentItem = "round";
-    this.cursorUrl = "assets/brush.png";
-    this.props.setAttribute("id", "tool-container");
-  }
-  static new(props) {
-    return new DrawingItemTool(props);
-  }
-  haldleOnClick(item, id, cursorUrl) {
-    return (_evt) => {
-      this.props.childNodes.forEach(node => {
-        if (node.id === id) {
-          node.style.opacity = ".5";
-        } else {
-          node.style.opacity = "1";
-        }
-      });
-      this.currentItem = item;
-      this.cursorUrl = cursorUrl;
-    };
-  }
-  resize({ width, height, top, left, overlayRect }) {
-    const props = this.props;
-    props.style.width = overlayRect.width * width + "px";
-    props.style.height = overlayRect.height * height + "px";
-    props.style.top = overlayRect.top + (overlayRect.height * top)
-      + "px";
-    props.style.left = overlayRect.left + (overlayRect.width * left)
-      + "px";
-  }
-  addItem(url, elementInput) {
-    var _a;
-    if (
-      !((_a = elementInput.innerProps) === null || _a === void 0
-        ? void 0
-        : _a.id)
-    ) {
-      throw AppError.new(
-        ErrorKind.ElementIdNotFoundError,
-        `tool item must have id`,
-      );
+    constructor(props) {
+        super("DrawingItemTool");
+        this.props = props;
+        this.currentItem = "round";
+        this.cursorUrl = "assets/brush.png";
+        this.props.setAttribute("id", "tool-container");
     }
-    const itemElement = createElement(`#${this.props.id}`, elementInput);
-    const imgElement = createElement(`#${elementInput.innerProps.id}`, {
-      tagName: "img",
-      innerProps: {
-        src: url,
-      },
-      style: {
-        padding: "10%",
-        width: "80%",
-        height: "80%",
-      },
-    });
-    itemElement.appendChild(imgElement);
-    const item = elementInput.innerProps["data-line-cap"];
-    itemElement.addEventListener(
-      "click",
-      this.haldleOnClick(item, itemElement.id, url),
-    );
-  }
+    static new(props) {
+        return new DrawingItemTool(props);
+    }
+    haldleOnClick(item, id, cursorUrl) {
+        return (_evt) => {
+            this.props.childNodes.forEach(node => {
+                if (node.id === id)
+                    node.style.opacity = ".5";
+                else
+                    node.style.opacity = "1";
+            });
+            this.currentItem = item;
+            this.cursorUrl = cursorUrl;
+        };
+    }
+    resize({ width, height, top, left, overlayRect }) {
+        const props = this.props;
+        props.style.width = overlayRect.width * width + "px";
+        props.style.height = overlayRect.height * height + "px";
+        props.style.top = overlayRect.top + (overlayRect.height * top)
+            + "px";
+        props.style.left = overlayRect.left + (overlayRect.width * left)
+            + "px";
+    }
+    addItem(url, elementInput) {
+        var _a;
+        if (!((_a = elementInput.innerProps) === null || _a === void 0 ? void 0 : _a.id)) {
+            throw AppError.new(ErrorKind.ElementIdNotFoundError, `tool item must have id`);
+        }
+        const itemElement = createElement(`#${this.props.id}`, elementInput);
+        const imgElement = createElement(`#${elementInput.innerProps.id}`, {
+            tagName: "img",
+            innerProps: {
+                src: url,
+            },
+            style: {
+                padding: "10%",
+                width: "80%",
+                height: "80%",
+            },
+        });
+        itemElement.appendChild(imgElement);
+        const item = elementInput.innerProps["data-line-cap"];
+        itemElement.addEventListener("click", this.haldleOnClick(item, itemElement.id, url));
+    }
 }
